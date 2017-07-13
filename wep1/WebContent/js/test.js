75bb98efc88ac360a163d0a9f13229b1e4fa3bf1
kohyeryeon <rhofus17@naver.com>
2017-07-13 18:38:15 +0900

    20170713

--- wep1/WebContent/js/test.js
+++ wep1/WebContent/js/test.js
@@ -4,18 +4,23 @@
 
 
 function checkValue(fObj){
-	var maxNum = fObj.elements.length;
+	var maxNum = fObj.elements.length;   //form테그 안에잇는 꼬봉들을 가져오기위한 변수 ->>elements
 	
 	var result = "";
 	for(var i=0;i<maxNum;i++){
 		var eObj= fObj.elements[i];
 		if(i%2==1 && i!=maxNum-1){
 			var checkNum = new Number(eObj.value);
-			if(isNaN(checkNum)){
-				alert("숫자가아냐!");
-				eObj.value = "";
-				eObj.focus();
-				return false;
+			try{  //자주써서 에러를 바로보자.
+				if(isNaN(checkNum)){
+					alert("숫자가아냐!");
+					eObj.value = "";
+					eObj.focus();
+					return false;
+				}
+			}catch(e){
+				alert(e);  
+				throw e;
 			}
 		}
 		if(eObj.value!="전송"){
